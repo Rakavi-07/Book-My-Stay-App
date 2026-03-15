7454354939b452ad93ed1b9d4eff19377735dbcd
Rakavi R <rrakavi25@gmail.com>
2026-03-15 19:04:23 +0530

    UC2: Basic room types with static availability

---
+++ src/DoubleRoom.java
@@ -0,0 +1,6 @@
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
---
+++ src/SingleRoom.java
@@ -0,0 +1,6 @@
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
